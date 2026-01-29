b2c1c292297c1e639270b023cc6392ca93664787
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2026-01-29 14:45:38 +0800

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = "mlt-serato-tags",
+  version = "1.0.0",
+  author = "Jan Holthuis",
+  author_email = "holthuis.jan@googlemail.com",
+  description = ("Serato DJ Pro GEOB tags documentation"),
+  license = "MIT",
+  url = "https://github.com/eidoriantan/serato-tags",
+  packages=find_packages(),
+  long_description=read('README.md'),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ]
+)
